0716c5547a9e50f8123df792e5f8edd342de8e5e
maaaaaaaaad <integral5137@gmail.com>
2024-03-22 21:37:09 +0900

    e

--- vpn/ContentView.swift
+++ vpn/ContentView.swift
@@ -1,21 +1,44 @@
-//
-//  ContentView.swift
-//  vpn
-//
-//  Created by mad on 3/22/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var id = 1
+    
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+
+    @State private var selected = "Archery"
+    
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Why not try")
+                .font(.largeTitle.bold())
         }
-        .padding()
+        
+        VStack {
+            Circle()
+                .fill(colors.randomElement() ?? colors[0])
+                .padding()
+                .overlay(
+                    Image(systemName: "figure.\(selected.lowercased())")
+                        .font(.system(size: 144))
+                        .foregroundColor(.white)
+                )
+            Text("\(selected)!")
+                .font(.title)
+        }
+        .transition(.slide)
+        .id(id)
+       
+    
+        VStack {
+            Button("Click"){
+                withAnimation(.easeInOut(duration: 0.5)) {
+                    selected = activities.randomElement() ?? activities[0]
+                    id += 1
+                }
+            }.buttonStyle(.borderedProminent)
+        }
     }
 }
 
