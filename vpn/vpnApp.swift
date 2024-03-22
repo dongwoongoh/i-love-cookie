7523cb9549f4cb05ddb4aeae298984bf0392cd30
maaaaaaaaad <integral5137@gmail.com>
2024-03-22 20:01:08 +0900

    Initial Commit

---
+++ vpn/vpnApp.swift
@@ -0,0 +1,17 @@
+//
+//  vpnApp.swift
+//  vpn
+//
+//  Created by mad on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct vpnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
