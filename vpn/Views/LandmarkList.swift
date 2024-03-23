72ab7cbf9c3eb4c0c93415e5a2fdc0579c38532a
maaaaaaaaad <integral5137@gmail.com>
2024-03-23 20:50:23 +0900

    e

---
+++ vpn/Views/LandmarkList.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+
+#Preview {
+    LandmarkList()
+}
